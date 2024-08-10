61dde0af6d4c5b0ac291a6e3ee315211a0f63180
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-08-10 16:20:06 +0530

    feat(SpinLock): use mutex instead of spin lock because mac doesn't have spin lock

--- SpinLock.h
+++ SpinLock.h
@@ -1,19 +1,19 @@
 #ifndef SPINLOCK_H_
 #define SPINLOCK_H_
 
-#include <pthread.h>
+#include <mutex>
 
 struct SpinLock {
-    pthread_spinlock_t sp;
-    SpinLock() noexcept { pthread_spin_init(&sp, PTHREAD_PROCESS_PRIVATE); }
+    std::mutex sp;
+    SpinLock() noexcept {  }
     SpinLock(const SpinLock&) = delete;
     SpinLock(SpinLock&&) = delete;
     SpinLock& operator=(const SpinLock&) = delete;
     SpinLock& operator=(SpinLock&&) = delete;
-    ~SpinLock() noexcept { pthread_spin_destroy(&sp); }
+    ~SpinLock() noexcept {  }
 
-    inline void lock() noexcept { pthread_spin_lock(&sp); }
-    inline void unlock() noexcept { pthread_spin_unlock(&sp); }
+    inline void lock() noexcept { sp.lock(); }
+    inline void unlock() noexcept { sp.unlock(); }
 };
 
 #endif /* SPINLOCK_H_ */
