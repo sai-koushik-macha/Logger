f934a9a4a02730913b12ca3343a0a4d8164cab0a
sai-koushik-macha <saikoushikmacha@gmail.com>
2024-11-03 10:43:04 +0530

    Use spinlock

--- SpinLock.h
+++ SpinLock.h
@@ -1,27 +1,19 @@
 #ifndef SPINLOCK_H_
 #define SPINLOCK_H_
 
-#include <atomic>
+#include <pthread.h>
 
 struct SpinLock {
-    std::atomic<bool> flag;
-    SpinLock() noexcept {  }
+    pthread_spinlock_t sp;
+    SpinLock() noexcept { pthread_spin_init(&sp, PTHREAD_PROCESS_PRIVATE); }
     SpinLock(const SpinLock&) = delete;
     SpinLock(SpinLock&&) = delete;
     SpinLock& operator=(const SpinLock&) = delete;
     SpinLock& operator=(SpinLock&&) = delete;
-    ~SpinLock() noexcept {  }
+    ~SpinLock() noexcept { pthread_spin_destroy(&sp); }
 
-    inline void lock() noexcept { 
-        while(1) {
-            if(!flag.exchange(true)) return;
-            while (flag.load())
-            {
-            }
-            
-        } 
-    }
-    inline void unlock() noexcept { flag.store(false); }
+    inline void lock() noexcept { pthread_spin_lock(&sp); }
+    inline void unlock() noexcept { pthread_spin_unlock(&sp); }
 };
 
 #endif /* SPINLOCK_H_ */
